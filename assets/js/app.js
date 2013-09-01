da4bed0341f76586cbb58b0c354935851ab91cd7
Sean Jones <sean@cssmonkey.co.uk>
2013-09-01 19:32:34 +0100

    image editor popup added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,52 +2,90 @@ window.EQTR = (function (module, $) {
     "use strict";
 
     $(function () {
-        module.createEmail(); 
+        $('.emailholder').load(function () {
+            module.emailEditor();
+        });
+        popup.init();
     });
 
-    module.createEmail = function () {
+    module.emailEditor = function () {
         var windowHeight = $(window).height();
+        var emailIframe = $('.emailholder');
 
+        // set the email iframe to be the same height as the browser window
         $('.emailholder').height(windowHeight);
 
-        var bindEvents = function () {
-            $('.createNewEmailLink').on('click', function (e) {
+
+        var editableImages = function () {
+            // edit alt attr
+            // edit src
+            // make clickable (add an anchor)
+            //  emailIframe.contents().find('img[data-editable="true"]').each
+            emailIframe.contents().find('img[data-editable="true"]').on('click', function () {
+                var imgSrc = $(this).attr('src'),
+                    altAttr = $(this).attr('alt'),
+                    imgPreview = '<div class="image"><img src="'+ imgSrc +'" alt="" /></div>',
+                    srcField = '<div class="field"><label for="imagesrc">Image Src</label><input name="imagesrc" type="text" value="' + imgSrc + '"/></div>',
+                    altField = '<div class="field"><label for="imagealt">Image Alt Attr</label><input name="imagealt" type="text" value="' + altAttr + '"/></div>',
+                    html = $('<div class="container" />');
+
+                html[0].innerHTML = imgPreview + srcField + altField;
+
+                popup.open(html);
+            });
+        }
+        editableImages();
+
+        var emailEditorControls = function () {
+
+            // Generate HTML
+            $('.generateHTML').on('click', function (e) {
                 e.preventDefault();
 
-                var frame = window.frames['emailholder'];
-                generateHTML(frame);
+                generateHTML();
             });
+
+            // Reset page 
             $('.resetLink').on('click', function (e) {
                 e.preventDefault();
 
                 resetPage();
             });
+
+            // Highlight editable regions
             $('.editableRegionsToggle').on('change', function (e) {
                 if ($(this).prop('checked')) {
-                    $('html').addClass('highlightRegions');
+                    emailIframe.contents().find('html').addClass('highlightRegions');
                 }
                 else {
-                    $('html').removeClass('highlightRegions');
+                    emailIframe.contents().find('html').removeClass('highlightRegions');
                 }
             });
-        }
-        bindEvents();
 
-        function resetPage() {
-            location.reload();
-        }
+            // Discard changes and reset the page
+            function resetPage() {
+                location.reload();
+            }
 
-        function generateHTML(frame) {
+            // Clean-up HTML and generate HTML markup
+            function generateHTML() {
 
-            var html = frame.document.documentElement.outerHTML;
+                // strip out any classnames or attrs added by editor interface
+                emailIframe.contents().find('html').removeClass('highlightRegions');
+                emailIframe.contents().find('[contenteditable]').removeAttr('contenteditable');
 
-            $('.textFieldHolder .htmlField').val(html);
-            if($('.textFieldHolder').is(':hidden')) {
-                $('.textFieldHolder').slideDown(function () {
-                    scrollPage($(this), 60);
-                });
+
+                var html = window.frames['emailholder'].document.documentElement.outerHTML;
+
+                $('.textFieldHolder .htmlField').val(html);
+                if ($('.textFieldHolder').is(':hidden')) {
+                    $('.textFieldHolder').slideDown(function () {
+                        scrollPage($(this), 60);
+                    });
+                }
             }
         }
+        emailEditorControls();
     }
 
 
@@ -55,9 +93,29 @@ window.EQTR = (function (module, $) {
     [ Reusable/Global Stuff ] 
     --------------------------------------------------- */
 
+    var popup = {
+        init: function () {
+            $('.modal').on('click', function () {
+                popup.close();
+            })
+        },
+        open: function (content) {
+            var $popup = $('.popup');
+
+            $('.content', $popup).html(content);
+            $popup.css({ marginTop: -($popup.height() / 2) });
+
+            $('html').addClass('showPopup');
+        },
+        close: function () {
+            $('html').removeClass('showPopup');
+            $('.popup').removeAttr('style').find('.contents').html('');
+        }
+    }
+
 
     /* Scroll to a specified element. Scroll postion can be offset by a specified amount. 
-       Element selector and offset are passed through as params. */
+    Element selector and offset are passed through as params. */
     var scrollPage = function (elem, offset) {
         var topPos = (elem ? elem.offset().top - offset : 0);
 
