6f01c630376e5ce55cbfeabe71c09376a71d9cb9
Sean Jones <sean.jones@eqtr.com>
2013-09-02 17:57:03 +0100

    editable images and links

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,24 +15,122 @@ window.EQTR = (function (module, $) {
         // set the email iframe to be the same height as the browser window
         $('.emailholder').height(windowHeight);
 
+        var editableAnchors = function () {
+            emailIframe.contents().find('a[data-editable="true"]').on('click', function (e) {
+                e.preventDefault();
+
+                var anchor = $(this),
+                    href = $(this).attr('href'),
+                    anchorText = $(this).text(),
+                    anchorPreview = '<div class="anchor"><a target="_blank" data-orighref="' + href + '" href="' + href + '">' + anchorText + '</a></div>',
+                    hrefField = '<div class="field"><label for="anchorhref">Link Href</label><input name="anchorhref" type="text" value="' + href + '"/></div>',
+                    textField = '<div class="field"><label for="anchortext">Link Text</label><input name="anchortext" type="text" value="' + anchorText + '"/></div>',
+                    submit = '<div class="anchorSave"><input type="submit" val="Save" / ></div>',
+                    html = $('<div class="container editanchor" />');
+
+                html[0].innerHTML = anchorPreview + hrefField + textField + submit;
+
+                popup.open(html);
+            });
+
+            $('.popup').on('keyup', '[name="anchorhref"]', function () {
+                var newHref = $(this).val();
+
+                $('.popup .anchor a').attr('href', newHref);
+            });
+
+            $('.popup').on('keyup', '[name="anchortext"]', function () {
+                var newText = $(this).val();
+
+                $('.popup .anchor a').text(newText);
+            });
+
+            $('.popup').on('click', '.anchorSave input', function (e) {
+                e.preventDefault();
+
+                var anchorHref = $('.popup .anchor a').data('orighref'),
+                    anchorElem = emailIframe.contents().find('a[href="' + anchorHref + '"]'),
+                    newHref = $('.popup [name="anchorhref"]').val(),
+                    newText = $('.popup [name="anchortext"]').val();
+
+                anchorElem.attr('href', newHref);
+                anchorElem.text(newText);
+
+                popup.close();
+            });
+
+        }
+        editableAnchors();
 
         var editableImages = function () {
             // edit alt attr
             // edit src
             // make clickable (add an anchor)
             //  emailIframe.contents().find('img[data-editable="true"]').each
-            emailIframe.contents().find('img[data-editable="true"]').on('click', function () {
-                var imgSrc = $(this).attr('src'),
+            emailIframe.contents().find('img[data-editable="true"]').on('click', function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                var img = $(this),
+                    imgSrc = $(this).attr('src'),
                     altAttr = $(this).attr('alt'),
-                    imgPreview = '<div class="image"><img src="'+ imgSrc +'" alt="" /></div>',
+                    link = $(this).parent(),
+                    linkHref = (link.attr('href')) ? link.attr('href') : '',
+                    imgPreview = '<div class="image"><a href="' + linkHref + '" target="_blank"><img data-origsrc="' + imgSrc + '" src="' + imgSrc + '" alt="" /></a></div>',
                     srcField = '<div class="field"><label for="imagesrc">Image Src</label><input name="imagesrc" type="text" value="' + imgSrc + '"/></div>',
                     altField = '<div class="field"><label for="imagealt">Image Alt Attr</label><input name="imagealt" type="text" value="' + altAttr + '"/></div>',
-                    html = $('<div class="container" />');
+                    hrefField = '<div class="field"><label for="imagehref">Link Href</label><input name="imagehref" type="text" value="' + linkHref + '"/></div>',
+                    submit = '<div class="imageSave"><input type="submit" val="Save" / ></div>',
+                    html = $('<div class="container editimage" />');
 
-                html[0].innerHTML = imgPreview + srcField + altField;
+                html[0].innerHTML = imgPreview + srcField + altField + hrefField + submit;
 
                 popup.open(html);
             });
+
+            $('.popup').on('keyup', '[name="imagesrc"]', function () {
+                var newSrc = $(this).val();
+
+                $('.popup .image img').attr('src', newSrc);
+            });
+
+            $('.popup').on('keyup', '[name="imagealt"]', function () {
+                var newAlt = $(this).val();
+
+                $('.popup .image img').attr('alt', newAlt);
+            });
+
+            $('.popup').on('keyup', '[name="imagehref"]', function () {
+                var newHref = $(this).val();
+
+                $('.popup .image a').attr('href', newHref);
+            });
+
+            $('.popup').on('click', '.imageSave input', function (e) {
+                e.preventDefault();
+
+                var imageSrc = $('.popup img').data('origsrc'),
+                    image = emailIframe.contents().find('img[src="' + imageSrc + '"]'),
+                    newImageSrc = $('.popup [name="imagesrc"]').val(),
+                    newImageAlt = $('.popup [name="imagealt"]').val(),
+                    newImageHref = $('.popup [name="imagehref"]').val();
+
+                image.attr('src', newImageSrc);
+                image.attr('alt', newImageAlt);
+
+                if (newImageHref && !image.parent('a').length > 0) {
+                    image.wrap('<a class="imageLink" data-editable="true" href="' + newImageHref + '" />')
+                }
+                else if (!newImageHref && image.parent('a').length) {
+                    image.parent('a').removeAttr('href');
+                }
+                else if (newImageHref && image.parent('a').length) {
+                    image.parent('a').attr('href', newImageHref);
+                }
+
+                popup.close();
+            });
+
         }
         editableImages();
 
@@ -73,6 +171,7 @@ window.EQTR = (function (module, $) {
                 // strip out any classnames or attrs added by editor interface
                 emailIframe.contents().find('html').removeClass('highlightRegions');
                 emailIframe.contents().find('[contenteditable]').removeAttr('contenteditable');
+                emailIframe.contents().find('[data-editable]').removeAttr('data-editable');
 
 
                 var html = window.frames['emailholder'].document.documentElement.outerHTML;
